41597f0ff986ff9ce136723543600569258faab3
Kirdankorabel <74896893+Kirdankorabel@users.noreply.github.com>
2023-02-13 18:59:00 +0200

    1
    1

---
+++ Assets/Window2.xaml.cs
@@ -0,0 +1,15 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+using System.Collections.Generic;
+using System.ComponentModel;
+#else
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+#endif
+
+namespace WpfApp2
+{
+}
